e5cfdb5c50cdf1fe7ddf1ba3e06453f8cd5e204d
Marshall Quander <marshall@quander.me>
2018-10-03 12:33:09 -0700

    More tweaks

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
+# note that -Os breaks the build :(
+EMFLAGS=--bind --closure 1 -Os -s MODULARIZE=1 -s 'EXPORT_NAME="Recast"' -s ENVIRONMENT=web -s ALLOW_MEMORY_GROWTH=1 -s 'EXPORTED_FUNCTIONS=["_buildNavmesh", "_loadContent", "_loadArray"]'
+
 recast.wasm: main/*.cpp recast/*.cpp
-	em++ --bind $^ -o recast.js -s MODULARIZE=1 -s 'EXPORT_NAME="Recast"' -s ENVIRONMENT=web -s ALLOW_MEMORY_GROWTH=1
+	EMCC_DEBUG=1 em++ ${EMFLAGS} -o recast.js $^
 
 .PHONY: clean
 clean:
--- main/port.cpp
+++ main/port.cpp
@@ -8,51 +8,51 @@
 #include "build.h"
 
 void loadArray(emscripten::val vs, emscripten::val fs) {
-  std::vector<float> vs_vec = emscripten::vecFromJSArray<float>(vs);
-  std::vector<int> fs_vec = emscripten::vecFromJSArray<int>(fs);
-  recast_loadArray(vs_vec.data(), vs_vec.size(), fs_vec.data(), fs_vec.size());
+    std::vector<float> vs_vec = emscripten::vecFromJSArray<float>(vs);
+    std::vector<int> fs_vec = emscripten::vecFromJSArray<int>(fs);
+    recast_loadArray(vs_vec.data(), vs_vec.size(), fs_vec.data(), fs_vec.size());
 }
 
 void loadContent(std::string content) {
-  recast_loadContent(content.c_str());
+    recast_loadContent(content.c_str());
 }
 
 emscripten::val buildNavmesh(
-  float cellSize,
-  float cellHeight,
-  float agentHeight,
-  float agentRadius,
-  float agentMaxClimp,
-  float agentMaxSlope,
-  float regionMinSize,
-  float regionMergeSize,
-  float edgeMaxLen,
-  float edgeMaxError,
-  float vertsPerPoly,
-  float detailSampleDist,
-  float detailSampleMaxError
+    float cellSize,
+    float cellHeight,
+    float agentHeight,
+    float agentRadius,
+    float agentMaxClimp,
+    float agentMaxSlope,
+    float regionMinSize,
+    float regionMergeSize,
+    float edgeMaxLen,
+    float edgeMaxError,
+    float vertsPerPoly,
+    float detailSampleDist,
+    float detailSampleMaxError
 ) {
-  std::string result(build(
-    cellSize,
-    cellHeight,
-    agentHeight,
-    agentRadius,
-    agentMaxClimp,
-    agentMaxSlope,
-    regionMinSize,
-    regionMergeSize,
-    edgeMaxLen,
-    edgeMaxError,
-    vertsPerPoly,
-    detailSampleDist,
-    detailSampleMaxError
-  ));
-  return emscripten::val(result);
+    std::string result(build(
+        cellSize,
+        cellHeight,
+        agentHeight,
+        agentRadius,
+        agentMaxClimp,
+        agentMaxSlope,
+        regionMinSize,
+        regionMergeSize,
+        edgeMaxLen,
+        edgeMaxError,
+        vertsPerPoly,
+        detailSampleDist,
+        detailSampleMaxError
+    ));
+    return emscripten::val(result);
 }
 
 EMSCRIPTEN_BINDINGS(recast)
 {
-  emscripten::function("build", &buildNavmesh);
-  emscripten::function("loadContent", &loadContent);
-  emscripten::function("loadArray", &loadArray);
+    emscripten::function("build", &buildNavmesh);
+    emscripten::function("loadContent", &loadContent);
+    emscripten::function("loadArray", &loadArray);
 }
